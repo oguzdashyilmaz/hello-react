26518b134d2814f390057af824c37507beeb19f1
oguzdashyilmaz <yilmazoguz@outlook.com>
2022-01-23 13:26:56 +0300

    Create User.js component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+const name = "Oğuz";
+const surname = "Yılmaz";
+const isLoggedIn = true;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>
+        {
+          isLoggedIn
+          ? `Benim adım ${name} ve soyadım ${surname}`
+          : `Giriş yapamadınız...`
+        }
+      </h3>
+    </>
   );
 }
 
---
+++ hello-react/src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function User() {
+    return (
+        <div>
+            This components works successfully...
+        </div>
+    )
+}
+
+export default User
