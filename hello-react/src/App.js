90147c8bbc769161660eb0fe8c7f2e52e333dced
oguzdashyilmaz <yilmazoguz@outlook.com>
2022-01-23 16:29:17 +0300

    Add friends as an array in the function of App

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,7 +4,12 @@ import User from "./components/User";
 function App() {
   return (
     <>
-      <User name="Oğuz" surname="Yılmaz" isLoggedIn={true} age={27}/>
+      <User 
+      name="Oğuz" 
+      surname="Yılmaz" 
+      isLoggedIn={false} 
+      age={27} 
+      friends={["veli", "ali", "hüseyin", "ayşe", "fatma", "hayriye"]}/>
     </>
   );
 }
--- hello-react/src/components/User.js
+++ hello-react/src/components/User.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-function User({name, surname, isLoggedIn, age}) {
+function User({ name, surname, isLoggedIn, age, friends }) {
     return (
         <>
             <h2>
                 {
-                    isLoggedIn 
-                    ? `${name} ${surname} ${age}`
-                    : "not logged in..."
+                    isLoggedIn
+                        ? `${name} ${surname} ${age}`
+                        : "not logged in..."
                 }
             </h2>
+            {
+                friends.map((friend, index) => <div key={index}>{index} - {friend}</div>)
+            }
             <ul>
                 <li><a href="#">Lorem, ipsum dolor.</a></li>
                 <li><a href="#">Porro, voluptas ut!</a></li>
